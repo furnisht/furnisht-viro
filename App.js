9138f7a8c511c2279eb2e6e3e0d367c91b4de5d8
Molly Lupton <molly.lupton@gmail.com>
2019-08-17 17:32:44 -0400

    refactored some code and started on furniture overlay

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
-import {
-  AppRegistry,
-  Text,
-  View,
-  Modal,
-  PixelRatio,
-  TouchableHighlight,
-  TouchableOpacity
-} from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import styles from "./styles";
 import { Instructions } from "./js/components/Instructions";
+import { FurnitureScreen } from "./js/components/FurnitureScreen";
 
 import { ViroARSceneNavigator } from "react-viro";
 
@@ -17,33 +10,27 @@ var sharedProps = {
   apiKey: "8C55396A-5FCB-4D94-B114-2F823C35529C"
 };
 
-var InitialARScene = require("./js/HelloWorldSceneAR");
+var InitialARScene = require("./js/components/HomeScreen");
 
-export default class ViroSample extends Component {
+export default class Main extends Component {
   constructor() {
     super();
 
     this.state = {
       navigatorType: "",
-      sharedProps: sharedProps
+      sharedProps: sharedProps,
+      furnishScreen: false
     };
 
     this._exitViro = this._exitViro.bind(this);
-    this.homePage = this.homePage.bind(this);
+    this.furnishButton = this.furnishButton.bind(this);
   }
+
+  furnishButton = () => {
+    this.setState({ furnishScreen: true });
+  };
 
   render() {
-    return this.homePage();
-  }
-
-  _exitViro() {
-    this.setState({
-      navigatorType: UNSET
-    });
-  }
-
-  homePage = () => {
-    // eslint-disable-next-line no-unused-expressions
     return (
       <View style={styles.outer}>
         <ViroARSceneNavigator
@@ -52,23 +39,31 @@ export default class ViroSample extends Component {
           initialScene={{ scene: InitialARScene }}
         />
         <Instructions />
+        <View isVirible />
         <View style={styles.navBar}>
           <TouchableOpacity>
             <Text style={styles.titleText}>Floor Plan</Text>
           </TouchableOpacity>
 
           <TouchableOpacity>
-            <Text style={styles.titleText}>Furnish</Text>
+            <Text style={styles.titleText} onPress={this.furnishButton}>
+              Furnish
+            </Text>
           </TouchableOpacity>
 
           <TouchableOpacity>
             <Text style={styles.titleText}>Project</Text>
           </TouchableOpacity>
         </View>
-        {/* // eslint-disable-next-line react/jsx-closing-tag-location */}
       </View>
     );
-  };
+  }
+
+  _exitViro() {
+    this.setState({
+      navigatorType: UNSET
+    });
+  }
 }
 
-module.exports = ViroSample;
+module.exports = Main;
---
+++ js/components/FurnitureScreen.js
@@ -0,0 +1,28 @@
+/* eslint-disable semi */
+import React from "react";
+import styles from "../../styles";
+import {
+  AppRegistry,
+  Text,
+  View,
+  Modal,
+  PixelRatio,
+  TouchableHighlight,
+  TouchableOpacity
+} from "react-native";
+import ImageOverlay from "react-native-image-overlay";
+
+export class FurnitureScreen extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>hi</Text>
+      </View>
+    );
+  }
+}
