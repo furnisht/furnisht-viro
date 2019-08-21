9e6bc89297f5241a50cb88ead4513a2670a5aedd
Molly Lupton <molly.lupton@gmail.com>
2019-08-21 10:47:13 -0400

    work from tuesday evening

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,12 @@
-const router = require('express').Router()
-module.exports = router
+const router = require("express").Router();
+module.exports = router;
 
-router.use('/users', require('./users'))
-router.use('/floorplan', require('./floorPlans'))
-router.use('/furniture', require('./furniture'))
+router.use("/users", require("./users"));
+router.use("/floorplans", require("./floorPlans"));
+router.use("/furniture", require("./furniture"));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
