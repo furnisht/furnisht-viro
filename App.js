606c7e4c36167f7482f5d3de45f1711edda4e2d6
Andrew Culkin <andrewsculkin@gmail.com>
2019-08-23 16:17:53 -0400

    adding 3d object that doesnt work

--- App.js
+++ App.js
@@ -218,10 +218,10 @@ export default class Main extends Component {
     for (let i = 0; i < pointsArr.length; i++) {
       let xPart = Math.pow(pointsArr[i].x - pointsArr[j].x, 2);
       let yPart = Math.pow(pointsArr[i].y - pointsArr[j].y, 2);
-      distances[i] = Math.sqrt(xPart+yPart);
+      distances[i] = Math.sqrt(xPart + yPart);
       j = i;
     }
-    return distances
+    return distances;
   }
 
   getArea(pointsArr, numPoints) {
